37d347d27455fb8348a8407b0d3c9e9f7ad7cf64
Loi Gloria <loigloria2001@gmail.com>
2020-03-22 20:20:08 +0300

    complete 7th  task

---
+++ task7.js
@@ -0,0 +1,36 @@
+const formidable = require("formidable");
+const fs = require("fs");
+const http = require("http");
+
+const port = 4001;
+
+const server = http.createServer(function(req, res) {
+  console.log(req.url, req.method);
+
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write(fs.readFileSync("index2.html"));
+    return res.end();
+  } else if (req.url === "/upload" && req.method === "POST") {
+    const form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+      console.log(files, files.filetoupload);
+      const oldpath = files.filetoupload.path;
+      const newpath =
+        "C:/glorichkaProduction/NodeJSTasks/files/" + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function(err) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+        res.write("Файл сохранен");
+        res.end();
+      });
+    });
+  }
+});
+
+server.listen(port, err => {
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+  console.log(`Server is listening on ${port}`);
+});
