fc5529343214d2848dcec3f21113f14c56a4150e
Loi Gloria <loigloria2001@gmail.com>
2020-03-24 18:02:44 +0300

    fixed 7th task

--- task7.js
+++ task7.js
@@ -1,27 +1,33 @@
-const formidable = require("formidable");
-const fs = require("fs");
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const formidable = require("formidable");
 
-const port = 4001;
+const port = 40001;
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.write(fs.readFileSync("index2.html"));
-    return res.end();
-  } else if (req.url === "/upload" && req.method === "POST") {
-    const form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-      const oldpath = files.filetoupload.path;
-      const newpath =
-        "C:/glorichkaProduction/NodeJSTasks/files/" + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function(err) {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-        res.write("Файл сохранен");
-        res.end();
+    res.end(fs.readFileSync("index2.html"));
+  } else if (req.method === "POST") {
+    let p = url.parse(req.url, true);
+
+    if (p.path === "/upload") {
+      const form = new formidable.IncomingForm();
+      form.parse(req, function(err, fields, files) {
+        const oldpath = files.filetoupload.path;
+        const newpath = __dirname + `\\files\\` + files.filetoupload.name;
+        console.log(newpath, oldpath);
+        fs.rename(oldpath, newpath, function(err) {
+          if (err) throw err;
+          res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+          res.end("Файл сохранен");
+        });
       });
-    });
+    } else if (p.path === "/CANCEL") {
+      res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("CANCEL");
+    }
   }
 });
 
@@ -29,5 +35,5 @@ server.listen(port, err => {
   if (err) {
     return console.log("Something bad happened", err);
   }
-  console.log(`Server is listening on ${port}`);
+  console.log(`Server started on http://localhost:${port}`);
 });
